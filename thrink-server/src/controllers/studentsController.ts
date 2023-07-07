062a57dbb8657aa92801a29092536c175a02155e
yashiroryo <>
2023-07-08 01:52:50 +0900

    feat: studentProfileを返す関数を実装

--- thrink-server/src/controllers/studentsController.ts
+++ thrink-server/src/controllers/studentsController.ts
@@ -1,12 +1,19 @@
 import { Request, Response, Router } from "express";
 import { StudentsRepository } from "./studentsRepository";
+import { StudentProfileModel } from "../models/studentProfileModel";
 
 export const studentsRouter = Router();
 const studentsRepo = new StudentsRepository();
+const studentProfileModel = new StudentProfileModel();
 
 // GET /v1/students
 studentsRouter.get("/", (req: Request, res: Response) => {
-  res.status(200).json(studentsRepo.getAllStudents());
+  console.log("pageIndex :" + req.query.pageIndex);
+  res.status(200).json({
+    studentProfile: studentProfileModel.getStudentProfile(
+      Number(req.query.pageIndex)
+    ),
+  });
 });
 
 // GET /v1/students/:id
---
+++ thrink-server/src/lib/pageNation.ts
@@ -0,0 +1,40 @@
+/**
+ * 該当するページのコンテンツを返す関数
+ * 存在しないページを指定した場合は1ページを返す
+ * @param array ページング済みの配列
+ * @param index 指定のページ数
+ */
+export const getTargetPage = (array: Array<Array<any>>, index: number) => {
+  if (array.length < index) {
+    return array[0];
+  }
+  return array[index - 1];
+};
+
+/**
+ * ページに分割する関数
+ * @param array tragetとなる配列
+ * @param pageContentsNum 1ページに何個コンテンツを格納するかを指定する変数
+ */
+export const splitPage = (
+  array: Array<any>,
+  pageContentsNum: number
+): Array<Array<any>> => {
+  // 処理結果が格納される配列
+  const results: Array<Array<any>> = [];
+  // 1ページのコンテンツが格納される配列
+  let result: Array<any> = [];
+  let num = 0;
+  array.forEach((v, i) => {
+    result.push(v);
+    num++;
+    if (num > pageContentsNum) {
+      num = 0;
+      results.push(result);
+      result = [];
+    } else if (i === array.length - 1) {
+      results.push(result);
+    }
+  });
+  return results;
+};
