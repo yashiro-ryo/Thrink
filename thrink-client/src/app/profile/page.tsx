08c3c9c6f97d64baaa74ab1a8268c02141bf0a9a
yashiroryo <>
2023-07-12 15:45:32 +0900

    feat: 自分のプロフィールを閲覧する機能を追加

--- thrink-client/src/app/profile/page.tsx
+++ thrink-client/src/app/profile/page.tsx
@@ -3,6 +3,10 @@ import { Card, Container, Button } from 'react-bootstrap'
 import NavbarComp from '@/components/ui-parts/Navbar/Navbar'
 import Footer from '@/components/ui-parts/Footer/Footer'
 import { styled } from 'styled-components'
+import { useEffect, useState } from 'react'
+import { useAppSelector } from '@/redux/hooks'
+import { useRouter } from 'next/navigation'
+import apiClient from '@/lib/http-common'
 
 const ContainerStyle = styled(Container)`
   margin-top: 50px;
@@ -10,6 +14,44 @@ const ContainerStyle = styled(Container)`
 `
 
 export default function UserProfile() {
+  const userProfileMeta = useAppSelector((state) => state.userProfileMetaReducer.profileMeta)
+  const router = useRouter()
+  // state
+  const [experience, setExperience] = useState('')
+  const [awards, setAwards] = useState('')
+  const [comment, setComment] = useState('')
+  const [links, setLinks] = useState('')
+  // method
+  const getProfile = (uid: number, userType: 0 | 1 | 2) => {
+    console.log(uid)
+    console.log(userType)
+    apiClient.get(`/v1/${getEndPointTarget(userType)}/${uid}`).then((res) => {
+      console.log(res)
+      if (userType === 0) {
+        setExperience(res.data.studentProfile.experience)
+        setAwards(res.data.studentProfile.awards)
+        setComment(res.data.studentProfile.comment)
+        setLinks(res.data.studentProfile.links)
+      }
+    })
+  }
+  const getEndPointTarget = (userType: 0 | 1 | 2) => {
+    if (userType === 0) {
+      return 'students'
+    } else if (userType === 1) {
+      return 'groups'
+    } else {
+      return ''
+    }
+  }
+  useEffect(() => {
+    if (userProfileMeta === null) {
+      // ログイン情報なし
+      router.push('/')
+    } else {
+      getProfile(userProfileMeta.uid, userProfileMeta.userType)
+    }
+  }, [userProfileMeta])
   return (
     <div>
       <NavbarComp />
@@ -18,12 +60,14 @@ export default function UserProfile() {
           <Card.Header>プロフィール</Card.Header>
           <Card.Body>
             <Button variant='primary'>編集する</Button>
-            <Card.Title>自己紹介</Card.Title>
-            <Card.Text></Card.Text>
-            <Card.Title>経歴</Card.Title>
-            <Card.Text></Card.Text>
+            <Card.Title>経験</Card.Title>
+            <Card.Text>{experience}</Card.Text>
+            <Card.Title>受賞歴</Card.Title>
+            <Card.Text>{awards}</Card.Text>
+            <Card.Title>コメント</Card.Title>
+            <Card.Text>{comment}</Card.Text>
             <Card.Title>リンク</Card.Title>
-            <Card.Text></Card.Text>
+            <Card.Text>{links}</Card.Text>
           </Card.Body>
         </Card>
       </ContainerStyle>
