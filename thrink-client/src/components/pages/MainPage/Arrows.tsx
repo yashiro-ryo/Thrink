569820b655c2ba0e7492c619dc94fb4fe4e66222
yashiroryo <>
2023-08-06 01:09:13 +0900

    feat: 矢印の色と長さを調整

--- thrink-client/src/components/pages/MainPage/Arrows.tsx
+++ thrink-client/src/components/pages/MainPage/Arrows.tsx
@@ -23,10 +23,10 @@ const ArrowContainerForMobile = styled.div`
 const ArrowLeft = styled.div`
   margin: 0 auto;
   position: relative; /*ボックスの位置指定 */
-  top: 40px;
-  width: 150px; /* ボックスの横幅を指定する */
+  top: 15px;
+  width: 200px; /* ボックスの横幅を指定する */
   height: 30px; /* ボックスの高さを指定する */
-  background: #666666; /* ボックスの背景色を指定する */
+  background: #ffcc66; /* ボックスの背景色を指定する */
   transform: rotate(45deg);
   &:after {
     position: absolute; /* 三角形の位置指定 */
@@ -35,16 +35,16 @@ const ArrowLeft = styled.div`
     right: -55px; /* 右から配置の基準位置を決める */
     border: 30px solid; /* 境界線を指定する */
     border-color: transparent; /* 境界線の色をなしにする */
-    border-left: 30px solid #666666; /* 境界線の上部を実線で指定する */
+    border-left: 30px solid #ffcc66; /* 境界線の上部を実線で指定する */
   }
 `
 const ArrowCenter = styled.div`
   margin: 0 auto;
   position: relative; /*ボックスの位置指定 */
-  top: 35px;
-  width: 100px; /* ボックスの横幅を指定する */
+  top: 20px;
+  width: 110px; /* ボックスの横幅を指定する */
   height: 30px; /* ボックスの高さを指定する */
-  background: #666666; /* ボックスの背景色を指定する */
+  background: #6699cc; /* ボックスの背景色を指定する */
   transform: rotate(90deg);
   &:after {
     position: absolute; /* 三角形の位置指定 */
@@ -53,16 +53,16 @@ const ArrowCenter = styled.div`
     right: -60px; /* 右から配置の基準位置を決める */
     border: 30px solid; /* 境界線を指定する */
     border-color: transparent; /* 境界線の色をなしにする */
-    border-left: 30px solid #666666; /* 境界線の上部を実線で指定する */
+    border-left: 30px solid #6699cc; /* 境界線の上部を実線で指定する */
   }
 `
 const ArrowRight = styled.div`
   margin: 0 auto;
   position: relative; /*ボックスの位置指定 */
-  top: 40px;
-  width: 150px; /* ボックスの横幅を指定する */
+  top: 15px;
+  width: 200px; /* ボックスの横幅を指定する */
   height: 30px; /* ボックスの高さを指定する */
-  background: #666666; /* ボックスの背景色を指定する */
+  background: #99cc33; /* ボックスの背景色を指定する */
   transform: rotate(135deg);
   &:after {
     position: absolute; /* 三角形の位置指定 */
@@ -71,7 +71,25 @@ const ArrowRight = styled.div`
     right: -50px; /* 右から配置の基準位置を決める */
     border: 30px solid; /* 境界線を指定する */
     border-color: transparent; /* 境界線の色をなしにする */
-    border-left: 30px solid #666666; /* 境界線の上部を実線で指定する */
+    border-left: 30px solid #99cc33; /* 境界線の上部を実線で指定する */
+  }
+`
+const ArrowCenterForMobile = styled.div`
+  margin: 0 auto;
+  position: relative; /*ボックスの位置指定 */
+  top: 20px;
+  width: 110px; /* ボックスの横幅を指定する */
+  height: 30px; /* ボックスの高さを指定する */
+  background: #99cc33; /* ボックスの背景色を指定する */
+  transform: rotate(90deg);
+  &:after {
+    position: absolute; /* 三角形の位置指定 */
+    content: ''; /* 三角形のコンテンツ */
+    top: -15px; /* 上部から配置の基準位置を決める */
+    right: -60px; /* 右から配置の基準位置を決める */
+    border: 30px solid; /* 境界線を指定する */
+    border-color: transparent; /* 境界線の色をなしにする */
+    border-left: 30px solid #99cc33; /* 境界線の上部を実線で指定する */
   }
 `
 
@@ -88,7 +106,7 @@ export default function Arrows() {
         <ArrowRight />
       </ArrowContainerForDesktop>
       <ArrowContainerForMobile>
-        <ArrowCenter />
+        <ArrowCenterForMobile />
       </ArrowContainerForMobile>
     </ArrowsContainer>
   )
