1525a22117415503ab5eb021a65feb5f6d3fca43
yashiroryo <>
2023-08-08 16:25:52 +0900

    fix: アイコンやヘッダー画像が変更されていない場合に空文字で上書きされないように実装

--- thrink-server/src/models/userProfileMetaModel.ts
+++ thrink-server/src/models/userProfileMetaModel.ts
@@ -10,8 +10,20 @@ export class UserProfileMetaModel {
   ) {
     console.log("update user icon header img");
     console.log(`${uid}-${iconImgUrl}-${headerImgUrl}`);
-    await db.query(
-      `update user_profile_meta set icon_img_url = '${iconImgUrl}', header_img_url = '${headerImgUrl}' where uid = ${uid}`
-    );
+    if (iconImgUrl.length === 0 && headerImgUrl.length > 0) {
+      await db.query(
+        `update user_profile_meta set header_img_url = '${headerImgUrl}' where uid = ${uid}`
+      );
+    } else if (headerImgUrl.length === 0 && iconImgUrl.length > 0) {
+      await db.query(
+        `update user_profile_meta set icon_img_url = '${iconImgUrl}' where uid = ${uid}`
+      );
+    } else if (iconImgUrl.length === 0 && headerImgUrl.length === 0) {
+      // do nothing
+    } else {
+      await db.query(
+        `update user_profile_meta set icon_img_url = '${iconImgUrl}', header_img_url = '${headerImgUrl}' where uid = ${uid}`
+      );
+    }
   }
 }
